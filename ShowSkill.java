c4d70c753da23a9853ffbf63033e196d810cd5fc
LJYhu <80735847+LJYhu@users.noreply.github.com>
2022-05-01 19:16:43 -0700

    Creating a failed tester

--- ShowSkill.java
+++ ShowSkill.java
@@ -1,3 +1,5 @@
 public class ShowSkill{
-
+    public static int multi(int a, int b){
+        return a*b;
+    }
 }
---
+++ ShowSkillTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ShowSkillTest {
+    @Test
+    public void multi() {
+        assertEquals(6, ShowSkill.multi(1, 1));
+    }
+
+}
